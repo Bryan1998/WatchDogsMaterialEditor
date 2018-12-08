f5dd7f59e30e2349e0fc8e9b8407c41c5cc9785e
Bryan Hernandez <pdnbryan1998@gmail.com>
2018-12-07 17:53:55 -0800

    Add files via upload

--- editor.py
+++ editor.py
@@ -1,34 +1,64 @@
-import re, zlib, pprint
+import re, zlib
 import xml.etree.ElementTree as ET
 from tkinter import *; from tkinter import filedialog
 
-nonascii = bytearray(range(0x80, 0x100))
-
 def byteswap(s):
     s = re.findall('..', s)
-    return ''.join(list(reversed(s)))
+    h = ''.join(list(reversed(s)))
+    return hex(int('0x' + h, 16))
 
 def crc32b(s):
     h = format(zlib.crc32(bytes(s, 'utf-8')), 'x')
-    return h
+    return byteswap(h)
 
 def readxml(f):
     tree = ET.parse(f)
     root = tree.getroot()
+    parameters = [[],[]]
     for parameter in root.findall('parameter'):
         name = parameter.get('name')
         type = parameter.get('type')
-        print(name,type)
+        parameters[0].append([crc32b(name)])
+        parameters[1].append([crc32b(type)])
+    return parameters[0]
 
 def readmat(filename):
-    with open(filename, mode='rb') as file: # b is important -> binary
+    with open(filename, mode='rb') as file:
         file.seek(0x48)
         name = file.read(0x17).rstrip(b'\0').decode('ascii', errors='ignore')
-        return re.sub(r'[^\w]', '', name)
+        name = re.sub(r'[^\w=]', '', name)
+    with open(filename, mode='rb') as file:
+        file.seek(0x60)
+        shader = file.read(0x1B).rstrip(b'\0').decode('ascii', errors='ignore')
+        shader = re.sub(r'[^\w=]', '', shader)
+    return [name, shader]
 
+    #for
+
+def btn(t):
+    global file, type
+    if t == "bin":
+        file = filedialog.askopenfilename(initialdir = ".", title = "Select file", filetypes = (("Watch Dogs Material File", ".material.bin"),))
+        type = "bin"
+    elif t == "xml":
+        file = filedialog.askopenfilename(initialdir = ".", title = "Select file", filetypes = (("XML File", "*.xml"),))
+        type = "xml"
+    else:
+        pass
 
 window = Tk()
-#xmlfile = filedialog.askopenfilename(initialdir = ".", title = "Select file", filetypes = (("XML File", "*.xml"),))
-binfile = filedialog.askopenfilename(initialdir = ".", title = "Select file", filetypes = (("Watch Dogs Material File", ".material.bin"),))
-#readxml(rootw.filename)
-print(readmat(binfile))
+
+btn_bin = Button(window, text=".material.bin", command=lambda: btn("bin"))
+btn_bin.grid(column=0, row=0)
+
+btn_xml = Button(window, text="materialdescriptor.xml", command=lambda: btn("xml"))
+btn_xml.grid(column=1, row=0)
+
+window.mainloop()
+
+if type == "bin":
+    print(readmat(file))
+elif type == "xml":
+    print(readxml(file))
+else:
+    print("Whoops!  Something isn't right...")
